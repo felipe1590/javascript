0d61e5f65a4bb2c7e9eb41afaec1add019114fd8
felipe1590 <felipe.gama.rk@gmail.com>
2023-05-05 18:43:21 -0300

    exercícios js

---
+++ aula11/ex008.js
@@ -0,0 +1,5 @@
+var vel = 59
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`MULTADO!`)
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+
+if (pais == 'Brasil') {
+    console.log(`Você é Brasileio!`)
+} else {
+    console.log(`Você é Estrangeiro!`)
+}
